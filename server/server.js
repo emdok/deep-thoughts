76b50c80854901fcfd2b2d654a69bdbd78c5bac3
Emily Dokken <dokkenemily@gmail.com>
2022-03-29 18:20:23 -0700

    add graphql playground plugin

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
+const {ApolloServerPluginLandingPageGraphQLPlayground} = require("apollo-server-core");
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -15,7 +16,12 @@ const startServer = async () => {
   const server = new ApolloServer({ 
     typeDefs, 
     resolvers, 
-    context: authMiddleware 
+    context: authMiddleware,
+    plugins: [
+
+      ApolloServerPluginLandingPageGraphQLPlayground(),
+  
+    ], 
   });
 
   // Start the Apollo server
